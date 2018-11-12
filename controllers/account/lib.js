6cd289e1550639b708b6f2e45addf1e5073c8a65
MathisZerbib <mathis.zerbib@gmail.com>
2018-11-12 03:53:32 +0100

    first-commit

---
+++ controllers/account/lib.js
@@ -0,0 +1,64 @@
+const User = require('../../schema/schemaUser.js');
+const passwordHash = require("password-hash");
+
+function signup(req, res) {
+    if (!req.body.email || !req.body.password) {
+        //Le cas où l'email ou bien le password ne serait pas soumit ou nul
+        res.status(400).json({
+            "text": "Requête invalide"
+        })
+    } else {
+        var user = {
+            email: req.body.email,
+            password: passwordHash.generate(req.body.password)
+        }
+        var findUser = new Promise(function (resolve, reject) {
+            User.findOne({
+                email: user.email
+            }, function (err, result) {
+                if (err) {
+                    reject(500);
+                } else {
+                    if (result) {
+                        reject(204)
+                    } else {
+                        resolve(true)
+                    }
+                }
+            })
+        })
+
+        findUser.then(function () {
+            var _u = new User(user);
+            _u.save(function (err, user) {
+                if (err) {
+                    res.status(500).json({
+                        "text": "Erreur interne"
+                    })
+                } else {
+                    res.status(200).json({
+                        "text": "Succès",
+                        "token": user.getToken()
+                    })
+                }
+            })
+        }, function (error) {
+            switch (error) {
+                case 500:
+                    res.status(500).json({
+                        "text": "Erreur interne"
+                    })
+                    break;
+                case 204:
+                    res.status(204).json({
+                        "text": "L'adresse email existe déjà"
+                    })
+                    break;
+                default:
+                    res.status(500).json({
+                        "text": "Erreur interne"
+                    })
+            }
+        })
+    }
+}
